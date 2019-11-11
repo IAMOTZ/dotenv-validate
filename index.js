7a4d70c96a99cc93adee46bd1af8a1952b6a3542
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2019-11-11 21:20:18 +0100

    Add type checking to the validation schema

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const chalk  = require('chalk');
+const exit = require('exit');
 
 let currentEnvironment = null;
 
@@ -10,14 +12,35 @@ let log = (message) => console.warn(`[dotenv-validate][WARNING] ${message}`);
 
 module.exports = (givenEnvVars, config = {}) => {
   const dotenvValidatePath = config.path || path.resolve(process.cwd(), '.env.validate');
-  // @todo: I need to validate the content of .evn.validate.js/json
-  const expectedEnvVars = require(dotenvValidatePath); 
 
-  currentEnvironment = config.currentEnv || process.env.NODE_ENV || 'development';
-  console.log('currentEnv not set in options or environment(NODE_ENV), using "development" as default');
-  log = config.log || log;
+  let expectedEnvVars;
+  try {
+    expectedEnvVars = require(dotenvValidatePath);
+  } catch(error) {
+    console.log('Invalid path to .env.validate');
+    // You should either have a .env.validate file at the directory of your package.json or specifity the full path to your .env.validate in config.path
+    exit(1);
+  }
 
-  validate(givenEnvVars.parsed, expectedEnvVars);
+  try {
+
+    if (!expectedEnvVars || expectedEnvVars.constructor.name !== 'Object') throw new InvalidateConfig('Invalid Validation. Your .env.validate.(js/json) file must be exporting an object');
+  
+    currentEnvironment = config.currentEnv || process.env.NODE_ENV || 'development';
+    console.log('currentEnv not set in options or environment(NODE_ENV), using "development" as default');
+    log = config.log || log;
+  
+    validate(givenEnvVars, expectedEnvVars);
+  } catch (error) {
+    if (error.path) {
+      const message = chalk.red(`${chalk.bold(error.path)}: ${error.message}`);
+      console.error(message);
+    } else {
+      console.log(chalk.red(error));
+    }
+    exit(1);
+  }
+
 };
 
 /**
@@ -29,24 +52,29 @@ const validate = (givenEnvVars, expectedEnvVars) => {
   const variableNames = Object.keys(expectedEnvVars);
 
   variableNames.forEach((name) => {
+    if (!expectedEnvVars[name] || expectedEnvVars.constructor.name !== 'Object') throw new InvalidateConfig({ path: name, message: 'This value must be an object' });
+
     const { env } = expectedEnvVars[name];
 
     let envIsObject = false;
 
-    if (env.constructor.name === 'Object') {
+    if (env && env.constructor.name === 'Object') {
       envIsObject = true;
-      if(!env[currentEnvironment]) return;
-    } else {
+      if (!env[currentEnvironment]) return;
+    } else if (Array.isArray(env)) {
       if (!env.includes(currentEnvironment)) return;
-    }
+    } else throw new InvalidateConfig({ path: `${name}.env`, message: `This value can either be an array or an Object, got ${typeof env} instead`});
 
     let {
       severityLevel, message, defaultValue,
     } = expectedEnvVars[name];
-  
-    if(envIsObject) {
+
+    if (envIsObject) {
       ({ severityLevel = severityLevel, defaultValue = defaultValue, message = message } = env[currentEnvironment])
     }
+
+
+    if(!severityLevel) throw new InvalidateConfig({ path: `${name}.severityLevel`, message: 'This value must be a string'});
 
     switch (severityLevel) {
       case 1: {
@@ -68,4 +96,12 @@ const validate = (givenEnvVars, expectedEnvVars) => {
       }
     }
   });
+}
+
+class InvalidateConfig extends Error {
+  constructor({ path, message }) {
+    super();
+    this.path = path;
+    this.message = message;
+  }
 }
